e15095fd118184af914bab2ad6289e5cfb96503c
happyiyate-hash <happyiyate@gmail.com>
2026-08-16 21:33:04 +0100

    fix: remove missing Vite keepAlive plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-import {keepAliveMobileView} from './build/keepAliveMobileView';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss(), keepAliveMobileView()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
